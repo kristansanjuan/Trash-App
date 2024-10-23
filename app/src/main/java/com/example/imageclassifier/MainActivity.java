d82bee2d0ad063e5937f7f8ae35219afc8b6675f
noobJhomz <jomarit.basario@gmail.com>
2024-10-23 09:31:57 +0800

    menu, and home contents
    changed the splash screen, created menu drawer, and home content

--- app/src/main/java/com/example/imageclassifier/MainActivity.java
+++ app/src/main/java/com/example/imageclassifier/MainActivity.java
@@ -2,35 +2,66 @@ package com.example.imageclassifier;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.Manifest;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.imageclassifier.ml.ModelUnquant;
+import com.google.android.material.navigation.NavigationView;
 
 import org.tensorflow.lite.DataType;
-import org.tensorflow.lite.schema.Model;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView result, confidence;
     ImageView imageView;
-    Button picture;
+    ImageButton picture;
     int imageSize = 224;
+    DrawerLayout drawerLayout;
+    ImageButton openMenu;
+    NavigationView navigationView;
+    ImageButton closeButton;
+    TextView titleArea;
+    TextView descriptionArea;
+    ImageButton selectedButton = null;
+
+    int[] oldIcon = {
+            R.drawable.biodegradable1,
+            R.drawable.infectious1,
+            R.drawable.hazardous1,
+            R.drawable.recyclable1,
+            R.drawable.nonbiodegradable1
+    };
+
+    int[] buttonColors = {
+            R.color.green,
+            R.color.orange,
+            R.color.red,
+            R.color.blue,
+            R.color.yellow
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +71,41 @@ public class MainActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
         confidence = findViewById(R.id.confidence);
         imageView = findViewById(R.id.imageView);
-        picture = findViewById(R.id.button);
+        picture = findViewById(R.id.cameraButton);
+        drawerLayout = findViewById(R.id.mainLayout);
+        openMenu = findViewById(R.id.openMenuButton);
+        navigationView = findViewById(R.id.navMenu);
+        titleArea = findViewById(R.id.titleArea);
+        descriptionArea = findViewById(R.id.descriptionArea);
+
+        ImageButton button1 = findViewById(R.id.biodegradableButton);
+        ImageButton button2 = findViewById(R.id.infectiousButton);
+        ImageButton button3 = findViewById(R.id.hazardousButton);
+        ImageButton button4 = findViewById(R.id.recyclableButton);
+        ImageButton button5 = findViewById(R.id.nonBiodegradableButton);
+
+        button1.setOnClickListener(view -> onButtonClick(button1, R.drawable.biodegradable2, 0, R.string.biodegradableTitle, R.string.biodegradableDescription));
+        button2.setOnClickListener(view -> onButtonClick(button2, R.drawable.infectious2, 1, R.string.infectiousTitle, R.string.infectiousDescription));
+        button3.setOnClickListener(view -> onButtonClick(button3, R.drawable.hazardous2, 2, R.string.hazardousTitle, R.string.hazardousDescription));
+        button4.setOnClickListener(view -> onButtonClick(button4, R.drawable.recyclable2, 3, R.string.recyclableTitle, R.string.recyclableDescription));
+        button5.setOnClickListener(view -> onButtonClick(button5, R.drawable.nonbiodegradable2, 4, R.string.nonBiodegradableTitle, R.string.nonBiodegradableDescription));
+
+        openMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.open();
+            }
+        });
+
+        View headerView = navigationView.getHeaderView(0);
+        closeButton = headerView.findViewById(R.id.closeMenuButton);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.close();
+            }
+        });
 
         picture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +120,80 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        List<ImageButton> wasteButtons = new ArrayList<>();
+        wasteButtons.add(findViewById(R.id.biodegradableButton));
+        wasteButtons.add(findViewById(R.id.infectiousButton));
+        wasteButtons.add(findViewById(R.id.hazardousButton));
+        wasteButtons.add(findViewById(R.id.recyclableButton));
+        wasteButtons.add(findViewById(R.id.nonBiodegradableButton));
+
+        startCircularAnimation(wasteButtons);
+    }
+
+    private void onButtonClick(ImageButton button, int newIcon, int index, int titleId, int descriptionId) {
+        if (selectedButton != null && selectedButton != button) {
+            revertButtonChanges(selectedButton);
+        }
+        button.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), buttonColors[index])));
+        button.setImageResource(newIcon);
+        selectedButton = button;
+
+        displayInfo(titleId, descriptionId);
+    }
+
+    private void revertButtonChanges(ImageButton button) {
+        int index = getButtonIndex(button);
+        if (index != -1) {
+            button.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.gray)));
+            button.setImageResource(oldIcon[index]);
+        }
+    }
+
+    private int getButtonIndex(ImageButton button) {
+        if (button.getId() == R.id.biodegradableButton) {
+            return 0;
+        } else if (button.getId() == R.id.infectiousButton) {
+            return 1;
+        } else if (button.getId() == R.id.hazardousButton) {
+            return 2;
+        } else if (button.getId() == R.id.recyclableButton) {
+            return 3;
+        } else if (button.getId() == R.id.nonBiodegradableButton) {
+            return 4;
+        }
+        return -1;
+    }
+
+    public void displayInfo(int titleId, int descriptionId) {
+        titleArea.setText(getString(titleId));
+        descriptionArea.setText(getString(descriptionId));
+
+        titleArea.setVisibility(View.VISIBLE);
+        descriptionArea.setVisibility(View.VISIBLE);
+    }
+
+    public void startCircularAnimation(List<ImageButton> buttons) {
+        ValueAnimator animator = ValueAnimator.ofFloat(0f, 360f);
+        animator.setDuration(10000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+
+        animator.addUpdateListener(animation -> {
+            float angle = (float) animation.getAnimatedValue();
+
+            for (int i = 0; i < buttons.size(); i++) {
+                ImageButton button = buttons.get(i);
+                float newAngle = (i * 72 + angle) % 360;
+
+                ConstraintLayout.LayoutParams params =
+                        (ConstraintLayout.LayoutParams) button.getLayoutParams();
+                params.circleAngle = newAngle;
+                button.setLayoutParams(params);
+            }
+        });
+
+        animator.start();
     }
 
     public void classifyImage(Bitmap image) {
--- app/src/main/java/com/example/imageclassifier/SplashScreenActivity.java
+++ app/src/main/java/com/example/imageclassifier/SplashScreenActivity.java
@@ -1,32 +1,57 @@
 package com.example.imageclassifier;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity{
+
+    private boolean imageSwitched = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        TextView textView = findViewById(R.id.textView);
-
         Animation popUpAnimation = AnimationUtils.loadAnimation(this, R.anim.pop_up_animation);
 
-        textView.startAnimation(popUpAnimation);
+        ImageView logoImage = findViewById(R.id.logo);
+        View rootLayout = findViewById(R.id.splashscreen_layout);
 
-        ImageView logoImage = findViewById(R.id.imageView3);
+        ObjectAnimator rotate1 = ObjectAnimator.ofFloat(logoImage, "rotation", 0f, 120f);
+        rotate1.setDuration(500);
+        rotate1.setStartDelay(500);
 
-        Animation rotationAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_animation);
+        ObjectAnimator rotate2 = ObjectAnimator.ofFloat(logoImage, "rotation", 120f, 240f);
+        rotate2.setDuration(500);
+        rotate2.setStartDelay(300);
 
-        logoImage.startAnimation(rotationAnimation);
+        ObjectAnimator rotate3 = ObjectAnimator.ofFloat(logoImage, "rotation", 240f, 360f);
+        rotate3.setDuration(500);
+        rotate3.setStartDelay(300);
+
+        rotate3.addUpdateListener(animation -> {
+            float rotationValue = (float) animation.getAnimatedValue();
+            if (rotationValue >= 270f && !imageSwitched) {
+                logoImage.setImageResource(R.drawable.ecosort_logo2);
+                animateBackgroundColor(rootLayout, Color.BLACK, Color.parseColor("#247D7F"));
+                imageSwitched = true;
+            }
+        });
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(rotate1, rotate2, rotate3);
+
+        animatorSet.start();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -35,6 +60,13 @@ public class SplashScreenActivity extends AppCompatActivity{
                 startActivity(intent);
                 finish();
             }
-        }, 3500); // Delay for 3 seconds
+        }, 3000); // Delay for 3 seconds
+    }
+    private void animateBackgroundColor(View view, int startColor, int endColor) {
+        ValueAnimator colorAnimation = ValueAnimator.ofArgb(startColor, endColor);
+        colorAnimation.setDuration(500);
+        colorAnimation.addUpdateListener(animator ->
+                view.setBackgroundColor((int) animator.getAnimatedValue()));
+        colorAnimation.start();
     }
 }
