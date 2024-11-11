a4b2c637debd04a42b58282d265bf05deea54b2d
A Gaming <nagatopain069@gmail.com>
2024-11-11 13:44:45 +0800

    change model_unquant.tflite change how waste result show added code for text and call for hazardous

--- app/src/main/java/com/example/imageclassifier/MainActivity.java
+++ app/src/main/java/com/example/imageclassifier/MainActivity.java
@@ -233,16 +233,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-            String[] classes = {"Biodegradable", "Non-biodegradable", "Recyclable"};//, "Biohazard"
+            String[] classes = {"Plastic Cups", "Papers", "Batteries", "Fruits", "Flammable", "Wood", "Aluminum", "Animal", "EcoBag", "Plastic Bag", "Organic Waste", "Face Masks", "Chemicals", "Juice Packs", "Leaves", "Books", "Clothes"};//Class 14 = Face Masks
 
-            result.setText(classes[maxPos]);
-
-            String s = "";
-            for(int i = 0; i < classes.length; i++){
-                s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100);
-            }
-
-            confidence.setText(s);
+            String detectedObject = classes[maxPos];
+            result.setText(detectedObject);
 
             // Releases model resources if no longer used.
             model.close();
