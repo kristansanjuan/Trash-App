17c38164c199800211db223c7ec6a6b23cc4fc8f
A Gaming <nagatopain069@gmail.com>
2025-02-27 10:56:00 +0800

    change versionCode, version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.imageclassifier"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.04"
+        versionCode = 5
+        versionName = "1.05"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
