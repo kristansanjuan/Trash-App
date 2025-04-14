0f864c0590137aaa442e20763a9a4ec0fdcea205
A Gaming <nagatopain069@gmail.com>
2025-04-14 09:58:29 +0800

    new uptodown app version, new model_unquant.tflite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.imageclassifier"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.07"
+        versionCode = 8
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
